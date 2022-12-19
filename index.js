66d1d27f8174be76d0fc1db17350494071be441f
Alexis Pizarro <78744202+apizarro1204@users.noreply.github.com>
2022-12-19 20:49:07 -0300

    ejemplo clase

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+app.use(express.static('public'))
+
+app.get('/saludo',(req,res)=>{
+  res.send('Hola bienvenido'+ process.env.NOMBRE)
+})
+
+app.listen(PORT, (err) => {
+  if (!err)
+    console.log(
+      `servidor escuchando en express ${PORT} - PID WORKER ${process.pid}`
+    )
+})
